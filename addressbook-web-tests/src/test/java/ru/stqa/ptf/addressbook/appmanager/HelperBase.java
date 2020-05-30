6f1fe281da84d4cae69e88b7a82a354bc08ab99a
Елена Васильева <lena.wasiljewa@mail.ru>
2020-05-31 00:03:40 +0300

    Доработан type в HelperBase

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/HelperBase.java
@@ -6,37 +6,42 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class HelperBase {
-    protected WebDriver wd;
+  protected WebDriver wd;
 
-    public HelperBase(WebDriver wd) {
-        this.wd = wd;
-    }
+  public HelperBase(WebDriver wd) {
+    this.wd = wd;
+  }
 
-    protected void click(By id, By locator) {
-        wd.findElement(locator).click();
-    }
+  protected void click(By id, By locator) {
+    wd.findElement(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        click(By.id("1"), locator);
+  protected void type(By locator, String text) {
+    click(By.id("1"), locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)) {
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
+      }
     }
+  }
 
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
 
-    public boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
+  public boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
     }
+  }
 }
