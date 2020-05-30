577fcc1d6e5ad6a281159ed6250d434307cb9822
Елена Васильева <lena.wasiljewa@mail.ru>
2020-05-30 22:49:29 +0300

    Создан класс Equation, тесты EquationTests, добавлен конструктор для выбора браузера при запуске тестов.

--- addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ptf/addressbook/appmanager/ApplicationManager.java
@@ -2,57 +2,73 @@ package ru.stqa.ptf.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-    public WebDriver wd;
+  public WebDriver wd;
 
-    private SessionHelper sessionHelper;
-    private NavigationHelper navigationHelper;
-    private GroupHelper groupHelper;
-    private ContactHelper contactHelper;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private ContactHelper contactHelper;
+  private String browser;
 
-    public void init() {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/");
-        groupHelper = new GroupHelper(wd);
-        navigationHelper = new NavigationHelper(wd);
-        sessionHelper = new SessionHelper(wd);
-        contactHelper = new ContactHelper(wd);
-        login("admin", "secret");
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
+
+  public void init() {
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
     }
 
-    public void login(String username, String password) {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(username);
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
+      wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      wd.get("http://localhost/addressbook/");
+      groupHelper = new GroupHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      sessionHelper = new SessionHelper(wd);
+      contactHelper = new ContactHelper(wd);
+      login("admin", "secret");
     }
 
-    public void stop() {
-        wd.quit();
+
+    public void login (String username, String password){
+      wd.findElement(By.name("user")).click();
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys(username);
+      wd.findElement(By.name("pass")).click();
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys(password);
+      wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    public void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-        wd.findElement(By.xpath("//html")).click();
+    public void stop () {
+      wd.quit();
     }
 
-    public GroupHelper getGroupHelper() {
-        return groupHelper;
+    public void logout () {
+      wd.findElement(By.linkText("Logout")).click();
+      wd.findElement(By.xpath("//html")).click();
     }
 
-    public NavigationHelper getNavigationHelper() {
-        return navigationHelper;
+    public GroupHelper getGroupHelper () {
+      return groupHelper;
     }
 
-    public ContactHelper getContactHelper() {
-        return contactHelper;
+    public NavigationHelper getNavigationHelper () {
+      return navigationHelper;
     }
-}
+
+    public ContactHelper getContactHelper () {
+      return contactHelper;
+    }
+  }
