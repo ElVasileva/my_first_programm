a868eef38a092b0f596d6bc1aa85aa03515d0a27
Елена Васильева <lena.wasiljewa@mail.ru>
2020-07-11 23:38:07 +0300

    Добавлен FtpHelper и LoginTests

--- mantis-tests/src/test/java/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/mantis/tests/TestBase.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
+
 public class TestBase {
-    
+
     protected static ApplicationManager app;
     protected WebDriver wd;
 
@@ -15,10 +18,12 @@ public class TestBase {
     public void setUp() throws Exception {
       app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() throws Exception {
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
     }
 
