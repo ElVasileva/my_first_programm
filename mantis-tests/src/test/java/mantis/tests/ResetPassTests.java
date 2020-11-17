1161a3e7c50a097d3a35831558e3a596733fdbed
Елена Васильева <lena.wasiljewa@mail.ru>
2020-11-17 18:55:30 +0300

    Задание 18

--- mantis-tests/src/test/java/mantis/tests/ResetPassTests.java
+++ mantis-tests/src/test/java/mantis/tests/ResetPassTests.java
@@ -1,18 +1,51 @@
 package mantis.tests;
 
-import mantis.model.MailMessage;
-import org.testng.annotations.Ignore;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import ru.lanwen.verbalregex.VerbalExpression;
+import mantis.model.MailMessage;
+import mantis.model.UserData;
+import mantis.model.Users;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
-import static java.lang.String.format;
 import static org.testng.Assert.assertTrue;
 
+
 public class ResetPassTests extends TestBase{
+
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
+    @Test
+    public void testRegistration() throws IOException, MessagingException {
+        Users users = app.db().usersWithoutAdmin();
+        UserData user = users.iterator().next();
+        String username = user.getName();
+        String email = user.getEmail();
+        String newPassword = "NewPass";
+
+        app.session().loginAsAdmin();
+        app.goTo().managePage();
+        app.goTo().manageUsersTab();
+        app.user().selectById(user.getId());
+        app.user().initPasswordReset();
+
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
+        String confirmationLink = app.mail().findConfirmationLink(email, mailMessages);
+        app.registration().finish(confirmationLink, newPassword);
+        assertTrue (app.newSession().login(username, newPassword));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
+
 
     @Ignore
     @Test
